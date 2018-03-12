ea9d35c0d2aa3ee0801f11b2ca002e6e64c499b6
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2018-03-12 15:13:39 +0000

    corrected test script

--- test.sh
+++ test.sh
@@ -1,3 +1,1 @@
-curl -H "Content-Type: application/json" -X POST -d '{"infile":{"title": {"text""
-: "Steep Chart"}, "xAxis": {"categories": ["Jan", "Feb", "Mar"]}, "series": [{"dd
-ata": [29.9, 71.5, 106.4]}]}}' 127.0.0.1:3001 -o testchart.png
+curl -H "Content-Type: application/json" -X POST -d '{"infile":{"title": {"text": "Steep Chart"}, "xAxis": {"categories": ["Jan", "Feb", "Mar"]}, "series": [{"data": [29.9, 71.5, 106.4]}]}}' 127.0.0.1:3001 -o testchart.png
